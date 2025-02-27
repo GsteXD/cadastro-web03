f84c2d49a1c35ed8cd45c91840bde84cdb51cc8d
Gustavo Tsuru <g.tsuruendo@gmail.com>
2025-02-26 21:35:36 -0300

    :construction: feat: Criação de uma tabela de rotas básica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 export const routes: Routes = [
-  {
+  { 
     path:'',
-    redirectTo: 'main-page',
+    redirectTo: 'mainPage', //Redirecionamento Padrão
     pathMatch:'full'
+  },
+  {
+    path:'cadastro',
+    component:CadastroComponent
+  },
+  {
+    path:'mainPage',
+    component:MainPageComponent
   }
 ];
