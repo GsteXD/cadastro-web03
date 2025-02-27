e206b2c99bff9743fb7cecfa978d17a7ab2b4a89
Gustavo Tsuru <g.tsuruendo@gmail.com>
2025-02-26 21:18:16 -0300

    :tada: init: Projeto Web - Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'main-page',
+    pathMatch:'full'
+  }
+];
