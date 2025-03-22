f2e3db04eaf34212456b5ca9396a0b14645cfa59
Gustavo Tsuru <g.tsuruendo@gmail.com>
2025-03-22 09:19:02 -0300

    :sparkles: feat: Página de detalhes do produto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { LoginComponent } from './components/login/login.component';
+import { DetalhesComponent } from './components/detalhes/detalhes.component';
 
 export const routes: Routes = [
   { 
@@ -22,5 +23,9 @@ export const routes: Routes = [
   {
     path:'mainPage',
     component:MainPageComponent
+  },
+  {
+    path:'detalhes/:id',
+    component:DetalhesComponent
   }
 ];
