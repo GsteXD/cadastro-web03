aa7b66607cc6efb9018f12ef3f9d2dade64b7bdf
Gustavo Tsuru <g.tsuruendo@gmail.com>
2025-03-30 21:56:42 -0300

    :sparkles: feat: Adicionando a finalização do pedido

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -27,7 +27,7 @@ export class CartService {
     this.cartVisible = !this.cartVisible; 
     console.log(this.cartVisible);
   }
-
+  //Adiciona o item no carrinho e incrementa o contador em 1
   addItem(produto: any) {
     let item = this.cartItems.find(p => p.id === produto.id);
     if (item) {
@@ -37,17 +37,18 @@ export class CartService {
     }
     console.log("Entrou")
   }
-
+  //Remove apenas um item do produto, diminuindo a contagem em 1
   removeOneItem(produto: any) {
     let item = this.cartItems.find(p => p.id === produto.id);
     if (item) {
       item.quantidade -= 1;
+      //Se chegar em 0, remove o produto do carrinho
       if (item.quantidade === 0) {
         this.removeItem(produto.id);
       }
     }
   }
-
+  //Remove todo o produto a partir de seu id
   removeItem(id: number) {
     this.cartItems = this.cartItems.filter(item => item.id !== id);
   }
@@ -62,4 +63,21 @@ export class CartService {
     return this.cartItems.reduce((total, item) => total + item.quantidade, 0);
   }
 
+  finalizarPedido() {
+    if (this.cartItems.length === 0) {
+        alert("Seu carrinho está vazio!");
+        return;
+    }    
+    // Log de verificação
+    console.log("Pedido gravado:", this.cartItems);
+
+    // Limpa o carrinho após a finalização
+    this.cartItems = [];
+    
+    // Exibe mensagem de sucesso
+    alert("Pedido finalizado com sucesso!");
+
+    // Esconde o carrinho
+    this.cartVisible = false;
+  }
 }
