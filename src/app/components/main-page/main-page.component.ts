aedb233b4763c66af66d2d2c6e9317b399c2c343
Gustavo Tsuru <g.tsuruendo@gmail.com>
2025-04-08 18:36:32 -0300

    :broom: Ajustes gerais

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { ProdutoService } from '../../services/produto/produto.service';
 import { Produto } from '../../models/produto.model';
@@ -8,11 +8,13 @@ import { Produto } from '../../models/produto.model';
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, CommonModule, CarouselComponent],
+  imports: [RouterLink, CommonModule, CarouselComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
-export class MainPageComponent {
+
+//Página principal do site, onde são exibidos os produtos
+export class MainPageComponent implements OnInit{
   produtos: Produto[][] = []; // Array unidimensional
 
   constructor(private produtoService: ProdutoService) { }
