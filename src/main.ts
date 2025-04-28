0f2a9e5f5cf1593117278c59fa0bdcc693f84480
Gustavo Tsuru <g.tsuruendo@gmail.com>
2025-04-27 22:45:06 -0300

    :ok_hand: fix: tornando o carrinho global

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppComponent } from './app/app.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes'; 
+import { CartService } from './app/services/cart/cart.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(withFetch()),
-    provideRouter(routes)
+    provideRouter(routes),
+    CartService
   ]
 }).catch((err) => console.error(err));
